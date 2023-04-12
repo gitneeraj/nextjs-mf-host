ab475020694050a8f70dbc4ec618225991e2ecf1
Neeraj <neeraj.a@nagarro.com>
2023-04-12 18:29:00 +0530

    ssr not working on federated catchall

--- components/shared/index.js
+++ components/shared/index.js
@@ -1,8 +1,9 @@
 const React = require('react');
-const createMatcher = require('feather-route-matcher');
-const { injectScript } = require('@module-federation/nextjs-mf/utils');
+import createMatcher from 'feather-route-matcher';
+const { injectScript } = require('@module-federation/utilities');
+
 const remoteVars = process.env.REMOTES || {};
-const remotes = Object.entries(remoteVars).reduce((acc, item) => {
+export const remotes = Object.entries(remoteVars).reduce((acc, item) => {
   const [key, value] = item;
   if (typeof value !== 'string') {
     acc[key] = {
@@ -20,6 +21,8 @@ const remotes = Object.entries(remoteVars).reduce((acc, item) => {
 }, {});
 
 async function matchFederatedPage(path) {
+  // console.log(remotes, 'shared');
+  console.log(remoteVars, 'remoteVars');
   const maps = await Promise.all(
     Object.keys(remotes).map(async remote => {
       const foundContainer = injectScript(remote);
@@ -46,7 +49,8 @@ async function matchFederatedPage(path) {
   }
 
   console.log(config);
-  const matcher = createMatcher.default(config);
+  const matcher = createMatcher(config);
+  console.log(matcher);
   return matcher(path);
 }
 
@@ -60,6 +64,8 @@ module.exports = {
         ...lazyProps,
         ...initialProps
       };
+      console.log(props);
+
       React.useEffect(() => {
         if (needsReload) {
           const runUnderlayingGIP = async () => {
@@ -82,7 +88,8 @@ module.exports = {
       }
 
       if (FederatedPage) {
-        return React.createElement(FederatedPage, props);
+        // return React.createElement(FederatedPage, props);
+        return <FederatedPage {...props} {...props.props} />;
       }
 
       return null;
@@ -95,11 +102,32 @@ module.exports = {
         // TODO: Run getInitialProps for error page
         return { renderError: true, ...props };
       }
-      if (!process.browser) {
-        return { needsReload: true, ...props };
+
+      const matchedPage2 = await matchFederatedPage(ctx.asPath);
+      const remote = matchedPage2?.value?.remote;
+      const mod = matchedPage2?.value?.module;
+
+      if (!remote || !mod) {
+        // TODO: Run getInitialProps for 404 page
+        return { render404: true, ...props };
       }
 
-      console.log('in browser');
+      const container = await injectScript(remote);
+      const FederatedPageWithGSSP = await container
+        .get(mod)
+        .then(factory => factory());
+
+      if (FederatedPageWithGSSP.getServerSideProps) {
+        const GSSP = await FederatedPageWithGSSP.getServerSideProps(ctx);
+        console.log(GSSP);
+        return { needsReload: true, ...props, ...GSSP, remotes };
+      }
+
+      if (!process.browser) {
+        return { needsReload: true, ...props, remotes };
+      }
+
+      console.log('in browser', ctx.asPath);
       const matchedPage = await matchFederatedPage(ctx.asPath);
 
       try {
@@ -129,7 +157,7 @@ module.exports = {
         };
         const federatedPageProps =
           (await FederatedPage.getInitialProps?.(modifiedContext)) || {};
-        return { ...federatedPageProps, FederatedPage };
+        return { ...federatedPageProps, FederatedPage, matchedPage };
       } catch (err) {
         console.log('err', err);
         // TODO: Run getInitialProps for error page
@@ -140,3 +168,145 @@ module.exports = {
     return FederatedCatchAll;
   }
 };
+// const React = require('react');
+// const createMatcher = require('feather-route-matcher');
+// const { injectScript } = require('@module-federation/nextjs-mf/utils');
+// const remoteVars = process.env.REMOTES || {};
+// const remotes = Object.entries(remoteVars).reduce((acc, item) => {
+//   const [key, value] = item;
+//   if (typeof value !== 'string') {
+//     acc[key] = {
+//       global: value
+//     };
+//     return acc;
+//   }
+//   const [global, url] = value.split('@');
+//
+//   acc[key] = {
+//     url,
+//     global
+//   };
+//   return acc;
+// }, {});
+//
+// async function matchFederatedPage(path) {
+//   const maps = await Promise.all(
+//     Object.keys(remotes).map(async remote => {
+//       const foundContainer = injectScript(remote);
+//       const container = await foundContainer;
+//
+//       return container
+//         .get('./pages-map')
+//         .then(factory => ({ remote, config: factory().default }))
+//         .catch(() => null);
+//     })
+//   );
+//
+//   const config = {};
+//
+//   for (const map of maps) {
+//     if (!map) continue;
+//
+//     for (let [path, mod] of Object.entries(map.config)) {
+//       config[path] = {
+//         remote: map.remote,
+//         module: mod
+//       };
+//     }
+//   }
+//
+//   console.log(config);
+//   const matcher = createMatcher.default(config);
+//   return matcher(path);
+// }
+//
+// module.exports = {
+//   matchFederatedPage,
+//   createFederatedCatchAll() {
+//     const FederatedCatchAll = initialProps => {
+//       const [lazyProps, setProps] = React.useState({});
+//
+//       const { FederatedPage, render404, renderError, needsReload, ...props } = {
+//         ...lazyProps,
+//         ...initialProps
+//       };
+//       React.useEffect(() => {
+//         if (needsReload) {
+//           const runUnderlayingGIP = async () => {
+//             const federatedProps = await FederatedCatchAll.getInitialProps(
+//               props
+//             );
+//             setProps(federatedProps);
+//           };
+//           runUnderlayingGIP();
+//         }
+//       }, []);
+//
+//       if (render404) {
+//         // TODO: Render 404 page
+//         return React.createElement('h1', {}, '404 Not Found');
+//       }
+//       if (renderError) {
+//         // TODO: Render error page
+//         return React.createElement('h1', {}, 'Oops, something went wrong.');
+//       }
+//
+//       if (FederatedPage) {
+//         return React.createElement(FederatedPage, props);
+//       }
+//
+//       return null;
+//     };
+//
+//     FederatedCatchAll.getInitialProps = async ctx => {
+//       // Bot marks "req, res, AppTree" as unused but those are vital to not get circular-dependency error
+//       const { err, req, res, AppTree, ...props } = ctx;
+//       if (err) {
+//         // TODO: Run getInitialProps for error page
+//         return { renderError: true, ...props };
+//       }
+//       if (!process.browser) {
+//         return { needsReload: true, ...props };
+//       }
+//
+//       console.log('in browser');
+//       const matchedPage = await matchFederatedPage(ctx.asPath);
+//
+//       try {
+//         console.log('matchedPage', matchedPage);
+//         const remote = matchedPage?.value?.remote;
+//         const mod = matchedPage?.value?.module;
+//
+//         if (!remote || !mod) {
+//           // TODO: Run getInitialProps for 404 page
+//           return { render404: true, ...props };
+//         }
+//
+//         console.log('loading exposed module', mod, 'from remote', remote);
+//         const container = await injectScript(remote);
+//         const FederatedPage = await container
+//           .get(mod)
+//           .then(factory => factory().default);
+//         console.log('FederatedPage', FederatedPage);
+//         if (!FederatedPage) {
+//           // TODO: Run getInitialProps for 404 page
+//           return { render404: true, ...props };
+//         }
+//
+//         const modifiedContext = {
+//           ...ctx,
+//           query: matchedPage.params
+//         };
+//         const federatedPageProps =
+//           (await FederatedPage.getInitialProps?.(modifiedContext)) || {};
+//         return { ...federatedPageProps, FederatedPage };
+//       } catch (err) {
+//         console.log('err', err);
+//         // TODO: Run getInitialProps for error page
+//         return { renderError: true, ...props };
+//       }
+//     };
+//
+//     return FederatedCatchAll;
+//   }
+// };
--- pages/[...slug].js
+++ pages/[...slug].js
@@ -1,25 +1,27 @@
-import dynamic from 'next/dynamic';
-const page = import('../realPages/[...slug]');
-const Page = dynamic(() => import('../realPages/[...slug]'));
+import { createFederatedCatchAll } from '@/components/shared';
 
-// Page.getInitialProps = async ctx => {
-//   const getInitialProps = (await page).default?.getInitialProps;
-//   console.log(getInitialProps(ctx));
-//   if (getInitialProps) {
-//     return getInitialProps(ctx);
+export default createFederatedCatchAll();
+
+// import dynamic from 'next/dynamic';
+// const page = import('remote1/pages/contact');
+// const Page = dynamic(() => import('remote1/pages/contact'));
+//
+// // Page.getInitialProps = async ctx => {
+// //   const getInitialProps = (await page).default?.getInitialProps;
+// //   console.log(getInitialProps(ctx));
+// //   if (getInitialProps) {
+// //     return getInitialProps(ctx);
+// //   }
+// //   return {};
+// // };
+// export async function getServerSideProps(context) {
+//   const fedPage = await page;
+//   console.log('slug', fedPage);
+//   if (fedPage.getServerSideProps) {
+//     return fedPage.getServerSideProps(context);
 //   }
-//   return {};
-// };
-export async function getServerSideProps(context) {
-  const fedPage = await page;
-  // console.log(fedPage.getServerSideProps);
-  if (fedPage.getServerSideProps) {
-    // console.log('asfasdfdsf');
-    return fedPage.getServerSideProps(context);
-  }
-  return {
-    props: {} // will be passed to the page component as props
-  };
-}
-
-export default Page;
+//   return {
+//     props: {} // will be passed to the page component as props
+//   };
+// }
+// export default Page;
